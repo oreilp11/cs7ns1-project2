b7b9f61222054776e925cda4de472d8c31090d58
Paul O'Reilly <oreilp11@tcd.ie>
2024-10-31 17:11:53 +0000

    fixed csv output format

--- src/rasp_classify.py
+++ src/rasp_classify.py
@@ -10,11 +10,12 @@ from utils import time_func
 @time_func
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('-n','--model-path', help='Name of the modedl to be used in classification on PI', type=str, required=True)
+    parser.add_argument('-m','--model-path', help='Name of the modedl to be used in classification on PI', type=str, required=True)
     parser.add_argument('-c','--captcha-dir', help='Path to the directory of the captchas stored to be classified on PI', type=str, required=True)
     parser.add_argument('-o','--output', help='File where the classifications should be saved', type=str, required=True)
     parser.add_argument('-s','--symbols', help='File with the symbols to use in captchas', type=str, required=True)
     parser.add_argument('-l','--labels', help='File with the labels to use in captchas', type=str, required=True)
+    parser.add_argument('-n','--shortname', help='Shortname for csv submission', type=str, required=True)
     args = parser.parse_args()
 
     if not os.path.exists(os.path.dirname(args.output)):
@@ -27,18 +28,23 @@ def main():
     labels_dict = {label:symbol for symbol, label in zip(captcha_symbols,captcha_labels)}
     print(f"Classifying captchas with symbol set {captcha_symbols} and labels {captcha_labels}")
 
+    results = []
+    model = tflite.Interpreter(args.model_path)
+    model.allocate_tensors()
+    for captcha in tqdm(os.listdir(args.captcha_dir)):
+        raw_data = cv2.imread(os.path.join(args.captcha_dir, captcha), 0)
+        h, w = raw_data.shape
+        raw_data = raw_data.reshape([-1, h, w, 1]).astype(np.float32)
+        model.set_tensor(model.get_input_details()[0]['index'], raw_data)
+        model.invoke()
+        prediction = np.array(model.get_tensor(model.get_output_details()[0]['index']))
+        prediction = np.argmax(prediction, axis=1)[0]
+        results.append((captcha,labels_dict[captcha_labels[prediction]]))
+    results.sort(key = lambda x:x[0])
     with open(args.output, 'w') as output_file:
-        model = tflite.Interpreter(args.model_path)
-        model.allocate_tensors()
-        for captcha in tqdm(os.listdir(args.captcha_dir)):
-            raw_data = cv2.imread(os.path.join(args.captcha_dir, captcha), 0)
-            h, w = raw_data.shape
-            raw_data = raw_data.reshape([-1, h, w, 1]).astype(np.float32)
-            model.set_tensor(model.get_input_details()[0]['index'], raw_data)
-            model.invoke()
-            prediction = np.array(model.get_tensor(model.get_output_details()[0]['index']))
-            prediction = np.argmax(prediction, axis=1)[0]
-            output_file.write(f'{captcha}, {labels_dict[captcha_labels[prediction]]}\n')
+        output_file.write(f'{args.shortname}\n')
+        for captcha, label in results:
+            output_file.write(f'{captcha},{label}\n')
 
 
 if __name__ == '__main__':
